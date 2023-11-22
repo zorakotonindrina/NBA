8bab59e84c6e2500a4ea176b123db22da449cf52
zorakotonindrina <120090215+zorakotonindrina@users.noreply.github.com>
2023-11-22 07:08:27 +0100

    mobile

---
+++ nbastat/capacitor.config.ts
@@ -0,0 +1,13 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'nbastat',
+  webDir: 'dist',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
+
